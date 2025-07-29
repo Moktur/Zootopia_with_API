0dea5a774de4228408b04361bdeeab5ce49d55b1
pepe <p.zajonz@posteo.de>
2025-07-29 09:14:40 +0200

    fixed issues: animals are now shown even if not all properties are there, website will show now the correct string when no animals where found

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -9,35 +9,53 @@ def load_data(file_path):
 
 
 def animal_data_to_html(animals_json_load):
+    """
+    extracts all animal data from a json file
+    returns a html string for all animals
+    """
     animal_info = ""
     for animal in animals_json_load:
         a_name, a_diet, a_location, a_type = extract_json_data(animal)
-        if a_name and a_diet and a_location and a_type:
-            animal_info += serialize_animal(a_name, a_diet, a_location, a_type)
+        # if a_name and a_diet and a_location and a_type:
+        animal_info += serialize_animal(a_name, a_diet, a_location, a_type)
     return animal_info
 
 
 def extract_json_data(animal_dict):
+    """
+    extract data from a single animal
+    returns four values about the animals
+    """
     a_name = animal_dict.get("name")
-    a_diet = animal_dict.get("characteristics", {}).get("diet")
-    a_locations = animal_dict.get("locations", [])
-    a_location = a_locations[0] if a_locations else None
-    a_type = animal_dict.get("characteristics", {}).get("type")
+    if animal_dict.get("characteristics", {}).get("diet") is not None:
+        a_diet = animal_dict.get("characteristics", {}).get("diet")
+    else:
+        a_diet = ""
+    if animal_dict.get("locations", []) is not None:
+        a_location = animal_dict.get("locations", [])[0]
+    else:
+        a_location = ""
+    if animal_dict.get("characteristics", {}).get("type") is not None:
+        a_type = animal_dict.get("characteristics", {}).get("type")
+    else:
+        a_type = ""
     return a_name, a_diet, a_location, a_type
 
 
 def serialize_animal(a_name, a_diet, a_location, a_type):
     """Creates HTML for each animal"""
-    return (
-        '<li class="cards__item">'
-        f'<div class="card__title">{a_name}</div>'
+    html_string = '<li class="cards__item">' \
+        f'<div class="card__title">{a_name}</div>' \
         f'<p class="card__text">'
-        f'<strong>Diet:</strong> {a_diet}<br/>'
-        f'<strong>Location:</strong> {a_location}<br/>'
-        f'<strong>Type:</strong> {a_type}</br>'
-        '</p>'
+    if a_diet != "":
+        html_string += f'<strong>Diet:</strong> {a_diet}<br/>'
+    if a_location != "":
+        html_string += f'<strong>Location:</strong> {a_location}<br/>'
+    if a_type != "":
+        html_string += f'<strong>Type:</strong> {a_type}</br>'
+    html_string += '</p>' \
         '</li>'
-    )
+    return html_string
 
 
 def write_html(animals_info):
@@ -57,11 +75,10 @@ def main():
         animals_data = data_fetcher.fetch_data(name)
 
         if not animals_data:
-            animals_info = f'<div class="error-message">'
-            f'{name} wasn\'t found in the database 🚽</div>'
+            animals_info = f'<div class="error-message">' \
+                f'{name} wasn\'t found in the database 🚽</div>'
         else:
             animals_info = animal_data_to_html(animals_data)
-
         write_html(animals_info)
         print("HTML file generated successfully!")
     except Exception as e:
